72e0bd8d4e4809e28e2cd6d18b5f190897d3ac7a
CodeMoiNgay <dangloctai00@gmail.com>
2024-10-26 15:45:30 +0700

    #Array

---
+++ Mang1c/Array_Karane.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	int n; cin >> n;
+	int a[n];
+	for(int &x : a){
+		cin >> x;
+	}
+	int res = -1e9, sum = 0;
+	for(int i = 0; i < n; i++){
+		sum += a[i];
+		res = max(res, sum);
+		if(sum < 0){
+			sum = 0;
+		}
+	}
+	cout << res;
+}
